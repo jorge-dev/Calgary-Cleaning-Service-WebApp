ec52c5d2065b93609f2ac5b4d6b53f4ee4d3881a
chrisoChen <47430791+chrisoChen@users.noreply.github.com>
2019-12-01 16:40:31 -0700

    created cleaners stuff
    put new css3 for cleaners 3

--- cleaner.php
+++ cleaner.php
@@ -5,8 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cleaner</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--===============================================================================================-->
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <!-- <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css"> -->
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+    <link rel="stylesheet" type="text/css" href="custom3.css" />
+    <title>Create Employee</title>
 </head>
 <body>
+    <?php
+    // fetch cleaner name
+    $name = "SELECT "
+    ?>
     <h1>Welcome Cleaner</h1>
+    <button type="button" onClick="alert('Hey, you pressed me!')">View Jobs</button>
+    <button type="button" onClick="alert('Hey, you pressed me!')">Eat Cake</button>  
 </body>
 </html>
